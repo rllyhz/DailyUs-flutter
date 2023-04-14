a7476037434cde5d56413ad9816189986ae78257
Rully Ihza Mahendra <rullyihza00@gmail.com>
2023-04-14 14:25:10 +0700

    Fixed up api client file

--- lib/data/datasources/remote/api_client.dart
+++ lib/data/datasources/remote/api_client.dart
@@ -2,9 +2,9 @@ import 'package:daily_us/common/secrets.dart';
 import 'package:dio/dio.dart';
 
 class DailyUsApiClient {
-  Dio? _client;
+  static Dio? _client;
 
-  Dio getInstance(AppSecret appSecret) {
+  static Dio getInstance(AppSecret appSecret) {
     _client ??= Dio(
       BaseOptions(
         connectTimeout: const Duration(seconds: 10),
