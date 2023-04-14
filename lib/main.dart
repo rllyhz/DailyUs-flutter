9a295969b235db1c231d77a0052e803388903fef
Rully Ihza Mahendra <rullyihza00@gmail.com>
2023-04-14 21:55:17 +0700

    Implemented router delegate

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:daily_us/common/localizations.dart';
 import 'package:daily_us/presentation/pages/splash_page.dart';
+import 'package:daily_us/routes/daily_us_router_delegate.dart';
 import 'package:flutter/material.dart';
 import 'package:daily_us/injection.dart' as di;
 
@@ -11,8 +12,22 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late DailyUsRouterDelegate appRouterDelegate;
+
+  @override
+  void initState() {
+    super.initState();
+
+    appRouterDelegate = DailyUsRouterDelegate();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,12 +38,9 @@ class MyApp extends StatelessWidget {
       ),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: SplashPage(
-        onAnimationEnd: () {
-          // check if user already logged in
-
-          // redirect to the corresponsing user state.
-        },
+      home: Router(
+        routerDelegate: appRouterDelegate,
+        backButtonDispatcher: RootBackButtonDispatcher(),
       ),
     );
   }
