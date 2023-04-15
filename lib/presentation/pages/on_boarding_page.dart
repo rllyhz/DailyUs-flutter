0404f48135557eab488ee7a3a5d2e6eb4c20df13
Rully Ihza Mahendra <rullyihza00@gmail.com>
2023-04-15 21:31:07 +0700

    Finished on boarding page

--- lib/presentation/pages/on_boarding_page.dart
+++ lib/presentation/pages/on_boarding_page.dart
@@ -1,3 +1,6 @@
+import 'package:daily_us/common/localizations.dart';
+import 'package:daily_us/common/ui/theme.dart';
+import 'package:daily_us/presentation/widgets/decorations/text_decorations.dart';
 import 'package:flutter/material.dart';
 
 class OnBoardingPage extends StatelessWidget {
@@ -14,23 +17,61 @@ class OnBoardingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var totalWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            const Text("OnBoardingPage"),
-            const SizedBox(
-              height: 24.0,
+      body: SizedBox.expand(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  const SizedBox(
+                    height: 28.0,
+                  ),
+                  Text(
+                    AppLocalizations.of(context)!.onBoardingHeading,
+                    style: onBoardingAppNameTextStyle(),
+                  ),
+                  const SizedBox(
+                    height: 24.0,
+                  ),
+                  Image.asset(
+                    "assets/illustration_on_boarding.png",
+                    semanticLabel: "OnBoarding logo",
+                    width: totalWidth / 1.55,
+                  ),
+                  const SizedBox(
+                    height: 24.0,
+                  ),
+                  Text(
+                    AppLocalizations.of(context)!.onBoardingSubHeading,
+                    style: onBoardingHeadlineTextStyle(),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 56.0,
+                  ),
+                  appButton(
+                    text: AppLocalizations.of(context)!.buttonLogin,
+                    onPressed: onLogin,
+                  ),
+                  const SizedBox(
+                    height: 8.0,
+                  ),
+                  appOutlinedButton(
+                    text: AppLocalizations.of(context)!.buttonRegister,
+                    onPressed: onRegister,
+                  ),
+                  const SizedBox(
+                    height: 24.0,
+                  ),
+                ],
+              ),
             ),
-            OutlinedButton(
-              onPressed: onRegister,
-              child: const Text("Register"),
-            ),
-            OutlinedButton(
-              onPressed: onLogin,
-              child: const Text("Login"),
-            ),
-          ],
+          ),
         ),
       ),
     );
