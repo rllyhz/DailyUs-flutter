3915039cf0d3e0ef0de37ab969119c77f309c6a7
Rully Ihza Mahendra <rullyihza00@gmail.com>
2023-04-17 13:46:25 +0700

    Created story item widget

---
+++ lib/presentation/widgets/daily_us_story_item.dart
@@ -0,0 +1,102 @@
+import 'package:daily_us/common/ui/colors.dart';
+import 'package:daily_us/domain/entities/story.dart';
+import 'package:daily_us/presentation/widgets/cards/daily_us_card.dart';
+import 'package:daily_us/presentation/widgets/cards/image_preview_card.dart';
+import 'package:daily_us/presentation/widgets/decorations/text_decorations.dart';
+import 'package:flutter/material.dart';
+
+class DailyUsStoryItem extends StatelessWidget {
+  const DailyUsStoryItem({
+    super.key,
+    this.onClick,
+    required this.padding,
+    required this.story,
+  });
+
+  final Story story;
+  final EdgeInsetsGeometry padding;
+  final void Function(Story)? onClick;
+
+  @override
+  Widget build(BuildContext context) {
+    var totalWidth = MediaQuery.of(context).size.width;
+    var previewCardwidth = totalWidth;
+    var previewCardHeight = totalWidth * 1 / 2;
+    if (totalWidth > 620.0) {
+      previewCardHeight = totalWidth * 1 / 3;
+    }
+
+    return InkWell(
+      onTap: () {
+        if (onClick != null) {
+          onClick!(story);
+        }
+      },
+      child: DailyUsCard(
+        padding: padding,
+        elevation: 0.0,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              width: previewCardwidth,
+              height: previewCardHeight,
+              child: ImagePreviewCard(
+                padding: const EdgeInsets.all(0.0),
+                child: Image.network(
+                  story.photoUrl,
+                  errorBuilder: (contex, _, err) => Container(
+                    width: double.maxFinite,
+                    height: double.maxFinite,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12.0),
+                      color: primaryColor,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 12.0,
+            ),
+            Row(
+              children: <Widget>[
+                Text(
+                  story.name,
+                  style: homeCardFullNameTextStyle(),
+                ),
+                const SizedBox(
+                  width: 8.0,
+                ),
+                SizedBox(
+                  width: 8.0,
+                  height: 8.0,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: primaryColor,
+                      borderRadius: BorderRadius.circular(24.0),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 8.0,
+                ),
+                Text(
+                  story.createdAt,
+                  style: homeCardDateTextStyle(),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 12.0,
+            ),
+            Text(
+              story.description,
+              style: homeCardDescriptionTextStyle(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
