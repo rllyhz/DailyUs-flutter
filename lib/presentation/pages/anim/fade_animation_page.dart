ee2bba9462d83272a665f19724a1fa0fa98f8638
Rully Ihza Mahendra <rullyihza00@gmail.com>
2023-04-23 21:59:17 +0700

    Updated fade_animation_page widget

--- lib/presentation/pages/anim/fade_animation_page.dart
+++ lib/presentation/pages/anim/fade_animation_page.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
 class FadeAnimationPage extends Page {
-  const FadeAnimationPage({super.key, required this.child});
+  const FadeAnimationPage({
+    super.key,
+    required this.child,
+    this.opaque = true,
+  });
 
   final Widget child;
+  final bool opaque;
 
   @override
   Route createRoute(BuildContext context) {
     return PageRouteBuilder(
+      opaque: opaque,
       settings: this,
       pageBuilder: (context, animation, secondAnim) {
         var curveTween = CurveTween(curve: Curves.easeIn);
