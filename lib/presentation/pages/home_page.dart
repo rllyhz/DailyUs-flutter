d513a0157c7b1dc0e9380d3c007d79bfbe3552e2
Rully Ihza Mahendra <rullyihza00@gmail.com>
2023-04-27 05:26:20 +0700

    Fixed up bugs when scroll to top in home page and other pages

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -11,6 +11,7 @@ import 'package:daily_us/presentation/widgets/shimmers/home_page_shimmer.dart';
 import 'package:fade_shimmer/fade_shimmer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 
 class HomePage extends StatefulWidget {
   static const valueKey = ValueKey("HomePage");
@@ -32,6 +33,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final ScrollController _scrollController = ScrollController();
+  bool _isCurrentlyVisible = true;
 
   @override
   void initState() {
@@ -42,11 +44,24 @@ class _HomePageState extends State<HomePage> {
       widget.controller!.scrollToTop = _scrollToTop;
     }
 
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels >=
+          _scrollController.position.maxScrollExtent) {
+        _onLoadMore(context);
+      }
+    });
+
     _onRefresh(context);
   }
 
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   void _onRefresh(BuildContext context) {
-    if (mounted) {
+    if (mounted && _isCurrentlyVisible) {
       context.read<HomeBloc>().add(
             OnFetchAllStoriesEvent(
               widget.authInfo.user.token,
@@ -55,12 +70,17 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  void _onLoadMore(BuildContext context) {
+    //
+  }
+
   void _refresh() {
     _onRefresh(context);
   }
 
   void _scrollToTop() {
-    if (mounted) {
+    // scroll only when home page is visible
+    if (mounted && _isCurrentlyVisible) {
       _scrollController.animateTo(
         0.0,
         duration: const Duration(milliseconds: 700),
@@ -74,130 +94,146 @@ class _HomePageState extends State<HomePage> {
     var totalWidth = MediaQuery.of(context).size.width;
 
     return SafeArea(
-      child: Container(
-        padding: const EdgeInsets.only(
-          left: screenPaddingSize,
-          right: screenPaddingSize,
-        ),
-        color: Colors.white,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const SizedBox(
-              height: 24.0,
-            ),
-            Text(
-              AppLocalizations.of(context)!.homeGreetingUser(
-                getFirstName(widget.authInfo.user.name),
+      child: VisibilityDetector(
+        key: const ValueKey('HomePage'),
+        onVisibilityChanged: (info) {
+          _isCurrentlyVisible = info.visibleFraction == 1;
+        },
+        child: Container(
+          padding: const EdgeInsets.only(
+            left: screenPaddingSize,
+            right: screenPaddingSize,
+          ),
+          color: Colors.white,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(
+                height: 24.0,
               ),
-              style: homeGreetingUserTextStyle(),
-            ),
-            const SizedBox(
-              height: 4.0,
-            ),
-            Text(
-              AppLocalizations.of(context)!.homeGreetingHeadline,
-              style: homeGreetingHeadlineTextStyle(),
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            Container(
-              width: totalWidth * 1 / 2,
-              height: 6.0,
-              decoration: BoxDecoration(
-                color: primaryColor,
-                borderRadius: BorderRadius.circular(12.0),
+              Text(
+                AppLocalizations.of(context)!.homeGreetingUser(
+                  getFirstName(widget.authInfo.user.name),
+                ),
+                style: homeGreetingUserTextStyle(),
               ),
-            ),
-            const SizedBox(
-              height: 12.0,
-            ),
-            BlocBuilder<HomeBloc, HomeState>(
-              builder: (context, state) {
-                if (state is HomeStateLoading) {
-                  return const Expanded(
-                    child: HomePageShimmer(
-                      padding: EdgeInsets.only(
-                        top: 12.0,
-                        bottom: 12.0,
-                      ),
-                      distanceBetweenItems: 12.0,
-                      fadeTheme: FadeTheme.light,
-                      totalItem: 10,
-                    ),
-                  );
-                } else if (state is HomeStateError) {
-                  return Column(
-                    children: <Widget>[
-                      const SizedBox(
-                        height: 32.0,
-                      ),
-                      Center(
-                        child: Text(
-                          getFailureMessage(
-                            context,
-                            state.failure,
-                          ),
+              const SizedBox(
+                height: 4.0,
+              ),
+              Text(
+                AppLocalizations.of(context)!.homeGreetingHeadline,
+                style: homeGreetingHeadlineTextStyle(),
+              ),
+              const SizedBox(
+                height: 8.0,
+              ),
+              Container(
+                width: totalWidth * 1 / 2,
+                height: 6.0,
+                decoration: BoxDecoration(
+                  color: primaryColor,
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+              ),
+              const SizedBox(
+                height: 12.0,
+              ),
+              BlocBuilder<HomeBloc, HomeState>(
+                builder: (context, state) {
+                  if (state is HomeStateLoading) {
+                    return const Expanded(
+                      child: HomePageShimmer(
+                        padding: EdgeInsets.only(
+                          top: 12.0,
+                          bottom: 12.0,
                         ),
-                      ),
-                      const SizedBox(
-                        height: 32.0,
-                      ),
-                      appOutlinedButton(
-                        text: AppLocalizations.of(context)!.buttonRefresh,
-                        onPressed: () {
-                          _onRefresh(context);
-                        },
-                      ),
-                    ],
-                  );
-                } else {
-                  var stories = (state as HomeStateHasData).stories;
-
-                  if (stories.isEmpty) {
-                    return Expanded(
-                      child: Center(
-                        child: Text(
-                          AppLocalizations.of(context)!.storiesEmptyMessage,
-                          style: homeCardDescriptionTextStyle(),
-                        ),
+                        distanceBetweenItems: 12.0,
+                        fadeTheme: FadeTheme.light,
+                        totalItem: 10,
                       ),
                     );
-                  } else {
-                    return Expanded(
-                      child: RefreshIndicator(
-                        color: secondaryColor,
-                        onRefresh: () async {
-                          _onRefresh(context);
-                        },
-                        child: ListView.builder(
-                          controller: _scrollController,
-                          padding: const EdgeInsets.only(
-                            top: 12.0,
-                            bottom: 12.0,
+                  } else if (state is HomeStateError) {
+                    return Column(
+                      children: <Widget>[
+                        const SizedBox(
+                          height: 32.0,
+                        ),
+                        Center(
+                          child: Text(
+                            getFailureMessage(
+                              context,
+                              state.failure,
+                            ),
                           ),
-                          itemCount: stories.length,
-                          itemBuilder: (context, index) {
-                            return Padding(
-                              padding: const EdgeInsets.only(bottom: 8.0),
-                              child: DailyUsStoryItem(
-                                padding: const EdgeInsets.all(12.0),
-                                story: stories[index],
-                                onClick: (story) {
-                                  widget.onDetail(story.id);
-                                },
-                              ),
-                            );
+                        ),
+                        const SizedBox(
+                          height: 32.0,
+                        ),
+                        appOutlinedButton(
+                          text: AppLocalizations.of(context)!.buttonRefresh,
+                          onPressed: () {
+                            _onRefresh(context);
                           },
                         ),
-                      ),
+                      ],
                     );
+                  } else {
+                    var stories = (state as HomeStateHasData).stories;
+
+                    if (stories.isEmpty) {
+                      return Expanded(
+                        child: Center(
+                          child: Text(
+                            AppLocalizations.of(context)!.storiesEmptyMessage,
+                            style: homeCardDescriptionTextStyle(),
+                          ),
+                        ),
+                      );
+                    } else {
+                      return Expanded(
+                        child: RefreshIndicator(
+                          color: secondaryColor,
+                          onRefresh: () async {
+                            _onRefresh(context);
+                          },
+                          child: ListView.builder(
+                            controller: _scrollController,
+                            padding: const EdgeInsets.only(
+                              top: 12.0,
+                              bottom: 12.0,
+                            ),
+                            itemCount: stories.length + 1,
+                            itemBuilder: (context, index) {
+                              if (index == stories.length) {
+                                return Center(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(12.0),
+                                    child: CircularProgressIndicator(
+                                      color: primaryColor,
+                                    ),
+                                  ),
+                                );
+                              }
+                              return Padding(
+                                padding: const EdgeInsets.only(bottom: 8.0),
+                                child: DailyUsStoryItem(
+                                  padding: const EdgeInsets.all(12.0),
+                                  story: stories[index],
+                                  onClick: (story) {
+                                    widget.onDetail(story.id);
+                                  },
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      );
+                    }
                   }
-                }
-              },
-            ),
-          ],
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
